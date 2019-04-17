1099deb5c527787506fb0895e64a2ebc82751bce
Juan Manuel Canals <juanmafcb2009@gmail.com>
2019-04-17 12:19:36 +0200

    First commit with new activities done.

---
+++ app/src/main/java/com/example/parkingapp/list_payment_methods.java
@@ -0,0 +1,13 @@
+package com.example.parkingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list_payment_methods extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_payment_methods);
+    }
+}
---
+++ app/src/main/java/com/example/parkingapp/qr_generator.java
@@ -0,0 +1,13 @@
+package com.example.parkingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class qr_generator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_generator);
+    }
+}
