cad525b03ff86e18ae57b9a21beb13d65e020399
adria <adria.caparros@estudiants.urv.cat>
2019-04-24 16:25:31 +0200

    commit

--- app/src/main/java/com/example/parkingapp/navmenu_choosepayment.java
+++ app/src/main/java/com/example/parkingapp/navmenu_choosepayment.java
@@ -1,13 +1,45 @@
 package com.example.parkingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class navmenu_choosepayment extends AppCompatActivity {
+
+    private ImageView Back;
+    private ImageView Credit;
+    private ImageView Paypal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navmenu_choosepayment);
+
+        Back = (ImageView)findViewById(R.id.ivBack);
+        Credit = (ImageView) findViewById(R.id.ivCredit);
+        Paypal = (ImageView) findViewById(R.id.ivPaypal);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(navmenu_choosepayment.this,FirstMenu.class));
+            }
+        });
+
+        Credit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(navmenu_choosepayment.this,add_credit_card.class));
+            }
+        });
+
+        Paypal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(navmenu_choosepayment.this,add_paypal.class));
+            }
+        });
     }
 }
