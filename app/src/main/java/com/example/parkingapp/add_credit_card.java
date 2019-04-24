ee68d6cbc1673cb2f658c64c89e9a00f201a1ac9
adria <adria.caparros@estudiants.urv.cat>
2019-04-24 16:40:41 +0200

    Credit payment

--- app/src/main/java/com/example/parkingapp/add_credit_card.java
+++ app/src/main/java/com/example/parkingapp/add_credit_card.java
@@ -1,13 +1,54 @@
 package com.example.parkingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class add_credit_card extends AppCompatActivity {
+
+    private ImageView Back;
+    private  ImageView Save;
+    private EditText CardHolder;
+    private EditText CardNumber;
+    private EditText YearMonth;
+    private EditText CVV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_credit_card);
+
+        Back = (ImageView)findViewById(R.id.ivBack);
+        Save = (ImageView)findViewById(R.id.ivSave);
+        CardHolder = (EditText) findViewById(R.id.etCardHolder);
+        CardNumber = (EditText) findViewById(R.id.etNumber);
+        YearMonth = (EditText) findViewById(R.id.etYearMonth);
+        CVV = (EditText) findViewById(R.id.etCVV);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(add_credit_card.this,navmenu_choosepayment.class));
+            }
+        });
+
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateCredit(CardHolder.getText().toString(),CardNumber.getText().toString(),YearMonth.getText().toString(),CVV.getText().toString());
+            }
+        });
+
+    }
+    private void validateCredit(String userCardHolder,String userCardNumber,String userYearMonth,String userCVV)
+    {
+        if((!userCardHolder.isEmpty()) && (!userCardNumber.isEmpty()) && (!userYearMonth.isEmpty()) && (!userCVV.isEmpty()))
+        {
+            Intent intent = new Intent(add_credit_card.this,FirstMenu.class);
+            startActivity(intent);
+        }
     }
 }
