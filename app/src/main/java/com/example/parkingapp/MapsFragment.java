36bb8ff107f334884a60a1d283b033b13266267c
Juan Manuel Canals <juanmafcb2009@gmail.com>
2019-05-02 21:36:19 +0200

    Added current location to main maps fragment.

--- app/src/main/java/com/example/parkingapp/MapsFragment.java
+++ app/src/main/java/com/example/parkingapp/MapsFragment.java
@@ -1,36 +1,57 @@
 package com.example.parkingapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+public class MapsFragment extends SupportMapFragment implements OnMapReadyCallback {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
-    @Nullable
+
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.content_navigation,null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+
+        getMapAsync(this);
+
+        return rootView;
     }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap map) {
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
-            boolean success = googleMap.setMapStyle(
+            boolean success = map.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
                             getActivity(), R.raw.mapstyle));
 
@@ -40,10 +61,30 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         } catch (Resources.NotFoundException e) {
             Log.e("Content", "Can't find style. Error: ", e);
         }
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(41, 1);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Tarragona"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Posicionar el mapa en una localización y con un nivel de zoom
+        LatLng tgn = new LatLng(41.118660, 1.245330);
+        // Un zoom mayor que 13 hace que el emulador falle, pero un valor deseado para
+        // callejero es 17 aprox.
+        float zoom = 13;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(tgn, zoom));
+
+        // Colocar un marcador en la misma posición
+        map.addMarker(new MarkerOptions().position(tgn));
+
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
     }
+
+
+
 
 }
